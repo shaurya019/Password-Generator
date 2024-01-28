7a214c91a96127eeab1c6a64f626e7703cbdbe3c
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-01-28 12:49:42 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState,useCallback,useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length,setLength] = useState(8);
-  const [numberAllowed,setNumberAllowed] = useState(false);
-  const [charAllowed,setCharAllowed] = useState(false);
-  const [password,setPassword] = useState('');
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const passwordRef = useRef(null)
 
   const generatePassword = useCallback(() => {
     let pass = ""
@@ -21,19 +25,21 @@ function App() {
 
     setPassword(pass)
 
-  }, [length, numberAllowed, charAllowed]);
+  }, [length, numberAllowed, charAllowed])
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+    
+  }
+
 
   useEffect(()=>{
     generatePassword()
   }, [length, numberAllowed, charAllowed])
 
-
-  const copyPasswordToClipboard = () => {
-    window.navigator.clipboard.writeText(password)
-  }
   return (
-    <>
-   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className='text-white text-center my-3'>Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
@@ -42,59 +48,54 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='Password'
         readOnly
-        // ref={passwordRef}
-        onClick={copyPasswordToClipboard}
+        ref={passwordRef}
         />
         <button
-        // onClick={copyPasswordToClipboard}
+        onClick={copyPasswordToClipboard}
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
-         </div>
+      </div>
       <div
       className='flex text-sm gap-x-2'
       >
         <div className='flex items-center gap-x-1'>
-        <input 
-        type="range"
-        min={6}
-        max={100}
-        value={length}
-        className='cursor-pointer'
-        onChange={
-          (e) => setLength(e.target.value)
-        }
-        name="" 
-        id=""
-         />
-         <label htmlFor="length">Length: {length}</label>
+          <input 
+          type="range" 
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e) => setLength(e.target.value)}
+          name="" 
+          id=""
+           />
+           <label htmlFor="length">Length: {length}</label>
         </div>
         <div className='flex items-center gap-x-1'>
-        <input 
-        type="checkbox"
-        defaultChecked={numberAllowed}
-        onChange={
-          () => setNumberAllowed((prev) => !prev)
-        }
-        name="" 
-        id=""
-         />
-         <label htmlFor="number">Number</label>
+          <input 
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          onChange={() => {
+            setNumberAllowed((prev) => !prev)
+          }}
+           name=""
+          id="" />
+          <label htmlFor="number">Numbers</label>
         </div>
         <div className='flex items-center gap-x-1'>
-        <input 
-        type="checkbox"
-        defaultChecked={charAllowed}
-        onChange={
-          () => setCharAllowed((prev) => !prev)
-        }
-        name="" 
-        id=""
-         />
-         <label htmlFor="char">Char</label>
+          <input 
+          type="checkbox"
+          defaultChecked={charAllowed}
+          onChange={() => {
+            setCharAllowed((prev) => !prev)
+          }}
+           name=""
+          id="" />
+          <label htmlFor="charInput">Character</label>
         </div>
+        
       </div>
     </div>
-    </>
   )
 }
 
